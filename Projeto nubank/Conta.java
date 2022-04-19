ee594fa760e7b03f22da8676079abb37c216f65a
Vinicius Smith <80126382+ViniSmith@users.noreply.github.com>
2022-04-19 12:03:20 -0300

    update
    Criando as classes

---
+++ Projeto nubank/Conta.java
@@ -0,0 +1,33 @@
+
+public class Conta
+{
+   private double saldo, creditoEspecial, saldoCreditoEspecial, numeroConta;
+   
+   public void setSaldo() {
+       this.saldo = saldo;
+   }
+   public double getSaldo() {
+       return this.saldo;
+   }
+   
+   public void setCreditoEspecial() {
+       this.creditoEspecial = creditoEspecial;
+   }
+   public double getCreditoEspecial() {
+       return this.creditoEspecial;
+   }
+   
+   public void setSaldoCreditoEspecial() {
+       this.saldoCreditoEspecial = saldoCreditoEspecial;
+   }
+   public double getSaldoCreditoEspecial() {
+       return this.saldoCreditoEspecial;
+   }
+   
+   public void setNumeroConta() {
+       this.numeroConta = numeroConta;
+   }
+   public double getNumeroConta() {
+       return this.numeroConta;
+   }
+}
