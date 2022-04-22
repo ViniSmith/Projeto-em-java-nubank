a34fbc23b8e287a0b06378a662a72a2130885d00
Vinicius Smith <80126382+ViniSmith@users.noreply.github.com>
2022-04-22 14:48:18 -0300

    update
    Criando a função de cadastrar cliente na classe principal

--- Projeto nubank/Principal.java
+++ Projeto nubank/Principal.java
@@ -4,7 +4,6 @@ public class Principal
 {
     static Scanner le = new Scanner(System.in);
     public static void main(String[] args) {
-        Cliente cliente = new Cliente();
         while(true) {
             int menu = menu();
             if(menu == 1) {
@@ -14,17 +13,34 @@ public class Principal
             }else if(menu == 3) {
                 
             }else if(menu == 4) {
+                
+            }else if(menu == 5) {
                 break;
             }
         }
     }
     
+    public static Cliente cadastrarConta() {
+        Cliente cliente = new Cliente();
+        System.out.println("Digite o seu nome");
+        cliente.setNome(le.next());
+        System.out.println("Digite o seu sobrenome");
+        cliente.setSobrenome(le.next());
+        System.out.println("Digite o seu CPF");
+        cliente.setCpf(le.nextInt());
+        System.out.println("Digite a sua data de nascimento");
+        cliente.setDataNascimento(le.nextInt());
+        
+        return cliente;
+    }
+    
     public static int menu() {
         System.out.println("Bem vindo ao Nubank, escola uma opção : ");
-        System.out.println("( 1 ) Cadastrar uma nova conta");
-        System.out.println("( 2 ) Realizar um depósito");
-        System.out.println("( 3 ) Realizar um saque");
-        System.out.println("( 4 ) Sair");
+        System.out.println("( 1 ) Cadastrar novo cliente");
+        System.out.println("( 2 ) Cadastrar uma nova conta");
+        System.out.println("( 3 ) Realizar um depósito");
+        System.out.println("( 4 ) Realizar um saque");
+        System.out.println("( 5 ) Sair");
         return le.nextInt();
     }
 }
